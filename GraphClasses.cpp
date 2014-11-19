41eb5c0e3b1f37c3f2c8ea9a40234a07a6aaca14
Adam St. Arnaud <ajstarna@ualberta.ca>
2014-11-18 19:35:57 -0700

    graph

--- GraphClasses.cpp
+++ GraphClasses.cpp
@@ -1,24 +1,8 @@
-//#include <vector>
-
-class Vertex;
-
+#include "GraphClasses.h"
+#include <vector>
 
 /////////////////    Edge class    ///////////////////////
-enum EdgeType {
-	MAY = 0,
-	MUST = 1
-};
 
-// no need to manually create edges. Use vertex methods for that
-class Edge {
-friend class Vertex;
-private:
-	Vertex * target;
-	EdgeType type;
-
-	Edge(Vertex * target, EdgeType type);
-
-};
 
 Edge::Edge(Vertex * target, EdgeType type)
 {
@@ -31,28 +15,6 @@ Edge::Edge(Vertex * target, EdgeType type)
 
 
 /////////////////    Vertex Class   //////////////////////
-class Vertex {
-private:
-	std::vector<Edge*> * outEdges;
-	std::vector<std::string> * variables;
-
-public:
-
-
-	// make a vertex with no outgoing edges
-	Vertex(std::string initialVariable);
-
-	// make a vertex with an outgoing edge to a target vertex
-	Vertex(std::string initialVariable, Vertex* initialTarget, EdgeType type);
-
-
-	// add an edge to a pre-constructed target Vertex
-	void addTarget(Vertex* targetVertex, EdgeType type);
-
-
-	// add an edge to a newly constructed vertex
-	void addTarget(std::string targetVar, EdgeType type);
-};
 
 
 Vertex::Vertex(std::string initialVariable)
@@ -63,7 +25,7 @@ Vertex::Vertex(std::string initialVariable)
 }
 
 
-Vertex::Vertex(std::string initialVariable, Vertex* initialTarget, EdgeType type)
+Vertex::Vertex(std::string initialVariable, Vertex* initialTarget, Edge::EdgeType type)
 {
 	this->outEdges = new std::vector<Edge*>();
 	Edge * newEdge = new Edge(initialTarget, type);
@@ -74,13 +36,13 @@ Vertex::Vertex(std::string initialVariable, Vertex* initialTarget, EdgeType type
 }
 
 
-void Vertex::addTarget(Vertex* targetVertex, EdgeType type)
+void Vertex::addTarget(Vertex* targetVertex, Edge::EdgeType type)
 {
 	Edge * outEdge = new Edge(targetVertex, type);
 	this->outEdges->push_back(outEdge);
 }
 
-void Vertex::addTarget(std::string targetVar, EdgeType type)
+void Vertex::addTarget(std::string targetVar, Edge::EdgeType type)
 {
 	Vertex * targetVertex = new Vertex(targetVar);
 	Edge * outEdge = new Edge(targetVertex, type);
@@ -90,22 +52,7 @@ void Vertex::addTarget(std::string targetVar, EdgeType type)
 
 
 /////////////////    Graph Class    ////////////////////////
-class Graph {
-private:
-	std::vector<Vertex*> * vertices;
 
-public:
-	// to add a pre-constructed vertex to the grapp
-	void addVertex(Vertex* newVertex);
-
-
-	// given a string variable and source and the edge type, this method will
-	// construct two new vertices, construct an edge between them, and add the
-	// vertices to the graph
-	void addVertices(std::string sourceVar, std::string targetVar, EdgeType type);
-	Graph();
-
-};
 
 Graph::Graph()
 {
@@ -117,7 +64,7 @@ void Graph::addVertex(Vertex* newVertex)
 	vertices->push_back(newVertex);
 }
 
-void Graph::addVertices(std::string sourceVar, std::string targetVar, EdgeType type)
+void Graph::addVertices(std::string sourceVar, std::string targetVar, Edge::EdgeType type)
 {
 	Vertex * targetVertex = new Vertex(targetVar);
 	Vertex * sourceVertex = new Vertex(sourceVar, targetVertex, type);
---
+++ GraphClasses.h
@@ -0,0 +1,78 @@
+#include <vector>
+
+
+
+
+class Vertex;
+
+
+/////////////////    Edge class    ///////////////////////
+
+
+// no need to manually create edges. Use vertex methods for that
+class Edge {
+friend class Vertex;
+public:
+	enum EdgeType {
+		MAY = 0,
+		MUST = 1
+	};
+
+private:
+	Vertex * target;
+	EdgeType type;
+
+	Edge(Vertex * target, EdgeType type);
+
+};
+
+
+/////////////////    Vertex Class   //////////////////////
+class Vertex {
+private:
+	std::vector<Edge*> * outEdges;
+	std::vector<std::string> * variables;
+
+public:
+
+
+	// make a vertex with no outgoing edges
+	Vertex(std::string initialVariable);
+
+	// make a vertex with an outgoing edge to a target vertex
+	Vertex(std::string initialVariable, Vertex* initialTarget, Edge::EdgeType type);
+
+
+	// add an edge to a pre-constructed target Vertex
+	void addTarget(Vertex* targetVertex, Edge::EdgeType type);
+
+
+	// add an edge to a newly constructed vertex
+	void addTarget(std::string targetVar, Edge::EdgeType type);
+};
+
+
+
+
+
+/////////////////    Graph Class    ////////////////////////
+class Graph {
+private:
+	std::vector<Vertex*> * vertices;
+
+public:
+	// to add a pre-constructed vertex to the grapp
+	void addVertex(Vertex* newVertex);
+
+
+	// given a string variable and source and the edge type, this method will
+	// construct two new vertices, construct an edge between them, and add the
+	// vertices to the graph
+	void addVertices(std::string sourceVar, std::string targetVar, Edge::EdgeType type);
+	Graph();
+
+};
+
+
+
+
